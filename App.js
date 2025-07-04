8e8e832cf8eda8697e78f6e6a69df458e6c41f24
Breno3ds2025 <breno.correa@escola.pr.gov.br>
2025-07-04 12:10:53 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 // import Aula01 from "./src/aula01";
-// import Aula03 from "./src/aula03";
-import TelaInicial from "./src/tela-inicial";
+import Aula02 from "./src/aula02";
+
 
 export default function App() {
   return (
     // <Aula01 />  
-    // <Aula03 />
-    <TelaInicial/>
+    <Aula02/>
   );
 }
--- src/aula02.jsx
+++ src/aula02.jsx
@@ -1,29 +1,49 @@
 import React, {Component} from "react";
-import {View, Text, TouchableOpacity, StyleSheet, Animated, Easing} from "react-native";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {View, Text, StyleSheet, Image} from "react-native";
 
-// cria componente animated para aceitar cores animadas
-const AnimatedIcon = Animated.createAnimatedComponent(Icon);
 
-class TelaPlanta extends Component{
+class Aula02 extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
     render() {
         return(
             <View style={styles.container}>
+                <Text>Aula02</Text>
+                <Image
+                    source={{uri:('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwb-sjuCUJSzG5K79FKAIWaIA6S9ymlX4Wzg&s')}}
+                    style={{width: 100, height: 100}}
+                />
 
+                <View style={styles.caixa}>1</View>
+                <View style={styles.caixa}>2</View>
+                <View style={styles.caixa}>3</View>
             </View>
         );
     }
 }
 
-export default TelaPlanta;
+export default Aula02;
 
 const styles = StyleSheet.create ({
 
     container: {
         flex: 1,
-        backgroundColor: '#b3e5fc',
-        alignItems: 'center',
         justifyContent: 'center',
+        alignItems: 'center',
     },
+
+    caixa: {
+        width: '80%',
+        height: 100,
+        backgroundColor: 'green',
+        borderRadius: 5,
+        margin: 5,
+    }
 
 })
